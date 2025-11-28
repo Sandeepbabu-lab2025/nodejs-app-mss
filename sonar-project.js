4c4b017b01bb164df41175cf80be13efeb2ed7be
Sandeepbabu0211 <sandeepbabuhn1718@gmail.com>
2025-11-28 11:16:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,9 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_9e1e270bd59cb6e35d4d7afc24b7ec2da38d7210',
+	    //'sonar.login': 'squ_9e1e270bd59cb6e35d4d7afc24b7ec2da38d7210',
+		   'sonar.login':admin'
+		    'sonar.password':'S@ndeepabd1718',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
